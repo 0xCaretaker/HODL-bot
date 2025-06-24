4ed40b8e44508bff5ef8c35454187f800662c1a7
Caretaker <63348904+0xCaretaker@users.noreply.github.com>
2025-06-24 13:29:11 +0530

    Update macd_bot.py

--- macd_bot.py
+++ macd_bot.py
@@ -1,6 +1,7 @@
 import yfinance as yf
 import requests
 import re
+import os
 from datetime import datetime
 
 # Fetch MACD-based action for each stock
@@ -79,8 +80,11 @@ def escape_md(text):
 
 # Send Telegram message
 def send_telegram_message(stock, action, interval, time, price):
-    TELEGRAM_TOKEN = "7785965061:AAEAXssnkbyj9vSVGHoCNegoUitePkZDK8U"
-    TELEGRAM_CHAT_ID = "794061838"
+    # TELEGRAM_TOKEN = "7785965061:AAEAXssnkbyj9vSVGHoCNegoUitePkZDK8U"
+    # TELEGRAM_CHAT_ID = "794061838"
+
+    TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+    TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
     url = f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage'
 
     emoji = {
