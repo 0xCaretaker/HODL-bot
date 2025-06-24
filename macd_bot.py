8123445aacf2dc602152814d5799f5d4c0b1710a
Caretaker <63348904+0xCaretaker@users.noreply.github.com>
2025-06-24 13:40:16 +0530

    Update macd_bot.py

--- macd_bot.py
+++ macd_bot.py
@@ -80,11 +80,11 @@ def escape_md(text):
 
 # Send Telegram message
 def send_telegram_message(stock, action, interval, time, price):
-    # TELEGRAM_TOKEN = "7785965061:AAEAXssnkbyj9vSVGHoCNegoUitePkZDK8U"
-    # TELEGRAM_CHAT_ID = "794061838"
+    TELEGRAM_TOKEN = "7785965061:AAEAXssnkbyj9vSVGHoCNegoUitePkZDK8U"
+    TELEGRAM_CHAT_ID = "794061838"
 
-    TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
-    TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+    # TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+    # TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
     url = f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage'
 
     emoji = {
